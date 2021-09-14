2dc5ae517ec511bfe91199fbd30bd0163f1331a3
dhouha <dhouha.mansour11@gmail.com>
2021-09-14 15:28:50 +0200

    observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { promise } from 'protractor';
-import { AppareilService } from './services/appareil.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  seconds:number;
 
   constructor(){}
+  ngOnInit() {
+    const counter = Observable.interval(1000);
+    counter.subscribe(
+      (value: number) => {
+        this.seconds = value;
+      },
+      (error: any) => {
+        alert('error');
+      },
+      () =>{
+        alert('conplete');
+      }
+    );
+  }
 
 }
