a3fb3c00699e79b10f9d822b3651ff6b9fc2b9e1
dhouha <dhouha.mansour11@gmail.com>
2021-09-14 10:08:45 +0200

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,37 +7,8 @@ import { AppareilService } from './services/appareil.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  isAuth = false;
-  appareils:any[];
-  lastUpdate = new Promise(
-    (resolve,reject) => {
-      const date = new Date();
-      setTimeout(
-        () => {
-          resolve(date);
-        }, 2000
-      )
-    }
-  );
+export class AppComponent {
 
-  constructor(private appareilService: AppareilService) {
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 4000
-    );
-  }
+  constructor(){}
 
-  ngOnInit(){
-    this.appareils = this.appareilService.appareils;
-  }
-
-  onAllumer() {
-    this.appareilService.switchOnAll();
-  }
-
-  onEteindre() {
-    this.appareilService.switchOffAll();
-  }
 }
