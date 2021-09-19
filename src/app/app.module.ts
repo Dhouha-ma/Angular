377d060742ba8e51794e94b16f6fd43b37c51f9a
dhouha <dhouha.mansour11@gmail.com>
2021-09-19 19:15:00 +0200

    forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,15 @@ import { SingleAppareilComponent } from './single-appareil/single-appareil.compo
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 import { AuthGuard } from './services/auth-guard.service';
 import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserService } from './services/user.service';
 
 const appRoutes : Routes = [
   { path: 'appareils', canActivate:[AuthGuard], component: AppareilViewComponent },
   { path: 'appareils/:id', canActivate:[AuthGuard], component: SingleAppareilComponent },
   { path: 'edit', canActivate:[AuthGuard], component: EditAppareilComponent },
   { path: 'auth', component: AuthComponent },
+  { path: 'user', component: UserListComponent },
   { path: '', component: AppareilViewComponent },
   { path: 'not-found', component: FourOhFourComponent },
   { path: '**', redirectTo:'/not-found' }
@@ -34,7 +37,8 @@ const appRoutes : Routes = [
     AppareilViewComponent,
     SingleAppareilComponent,
     FourOhFourComponent,
-    EditAppareilComponent
+    EditAppareilComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
@@ -45,7 +49,8 @@ const appRoutes : Routes = [
   providers: [
     AppareilService,
     AuthService,
-    AuthGuard
+    AuthGuard,
+    UserService
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,27 @@
+import { Subject } from "rxjs/Subject";
+import { User } from "../models/User.model";
+
+export class UserService {
+    private users: User[] = [
+        {
+            firstName:'James',
+            lastName:'Smith',
+            email:'test.test@gmail.com',
+            drinkPreference:'Coca',
+            hobbies: [
+                'coder',
+                'dancer'
+            ]
+        }
+    ];
+    userSubject = new Subject<User[]>();
+
+    emitUser(){
+        this.userSubject.next(this.users.slice());
+    }
+
+    addUser(user: User){
+        this.users.push(user);
+        this.emitUser();
+    }
+}
